87072ae554bb2fd18acf60a3eb7a07d56441b161
StrangeCow05 <alex.miller@bsu.edu>
2025-02-20 23:30:05 +0000

    im so done

--- date.cpp
+++ date.cpp
@@ -11,5 +11,16 @@ Date::Date(){
 }//end constructer
 
 void Date::init (std::string dateString){
+ std::stringstream converter;
 
+ std::string sMonth;
+ std::string sDay;
+ std::string sYear;
+
+ int month;
+ int day;
+ int year;
+
+ converter.clear();
+ converter.str(dateString);
 }
