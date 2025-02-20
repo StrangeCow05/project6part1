ae69f1672b7aac0e96a008506d4f3b44a2836ac9
StrangeCow05 <alex.miller@bsu.edu>
2025-02-20 20:51:10 +0000

    main and address.h added

---
+++ address.h
@@ -0,0 +1,17 @@
+#include <string>
+
+class Address{
+        private:
+                std::string street;
+                std::string town;
+                std::string state;
+                std::string zip;
+
+        public:
+               Address();
+               void init(street,town,state,zip);
+               void printDate();
+
+};//end Address
+~                                                                    
+~                   
