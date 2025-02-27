dc622f460bd040aecfbae394bbb2dc13d37498a6
StrangeCow05 <alex.miller@bsu.edu>
2025-02-27 16:34:26 +0000

    started on student.cpp

---
+++ student.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "student.h"
+#include "address.h"
+#include "date.h"
+
+Student::Student(){
+	Student::studentString
+
+
+}//end constructor 
